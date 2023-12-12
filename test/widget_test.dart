c2e89551456ecf085c9c11438ae45e888e6bb57b
HARSHITA-1.0 <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2023-12-12 23:20:06 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:pro/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    Build our app and trigger a frame.
     await tester.pumpWidget(const Sarvayon());
 
     // Verify that our counter starts at 0.
