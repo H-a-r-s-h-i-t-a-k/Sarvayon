ce803dd82ca36f7762af5fb0a91608a4d9b43d6a
HARSHITA-1.0 <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2023-12-12 23:23:09 +0530

    Update home.dart

--- lib/module/home-screens/home.dart
+++ lib/module/home-screens/home.dart
@@ -1,6 +1,6 @@
-import 'package:awesome_dialog/awesome_dialog.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:awesome_dialog/awesome_dialog.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:get/get.dart';
