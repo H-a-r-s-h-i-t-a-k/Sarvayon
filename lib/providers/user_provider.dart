58ed4fb09cb13a5c79fefac5c9975f6fbb92275d
HARSHITA-1.0 <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2023-12-23 20:13:17 +0530

    Update user_provider.dart

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 import '../models/user.dart';
 
 class UserProvider extends ChangeNotifier {
-  // User _user = User(
-  //   id: '',
-  //   name: '',
-  //   email: '',
-  //   password: '',
-  //   address: '',
-  //   type: '',
-  //   token: '',
-  // );
+  User _user = User(
+    id: '',
+    name: '',
+    email: '',
+    password: '',
+    address: '',
+    type: '',
+    token: '',
+  );
   User get user => _user;
 
   void setUser(String user) {
