8c39b2c1db782a08f82b66ad78bf39057409b4ba
HARSHITA-1.0 <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-01-08 19:06:13 +0530

    Update AnimatedBtn.dart

--- lib/constants/AnimatedBtn.dart
+++ lib/constants/AnimatedBtn.dart
@@ -1,4 +1,4 @@
-// import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
