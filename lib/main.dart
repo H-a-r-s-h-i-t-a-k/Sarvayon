ac49995ebfd766b03c4dfe35595db63f320abf90
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-06-25 02:29:00 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,11 @@ import 'package:pro/router.dart';
 import 'package:provider/provider.dart';
 void main() {
 
-  // runApp(MultiProvider(providers: [
-  //   ChangeNotifierProvider(
-  //     create: (context) => UserProvider(),
-  //   ),
-  // ], child: const Sarvayon()));
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+    ),
+  ], child: const Sarvayon()));
 }
 
 class Sarvayon extends StatefulWidget {
